d7d6d4d999c1d3840dad732eab578d07b8ab626c
HungNX247 <hungnx6666@gmail.com>
2026-02-09 11:31:25 +0700

    add

---
+++ LESSON02/practice/spring-timezone-maven/src/main/java/com/codegym/controller/TimeController.java
@@ -0,0 +1,33 @@
+package com.codegym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.TimeZone;
+
+@Controller
+public class TimeController {
+
+    @GetMapping("/world-clock")
+    public String getTimeByTimezone(ModelMap modelMap, @RequestParam(name = "city", required = false, defaultValue = "Asia/Ho_Chi_Minh") String city) {
+        Date date = new Date();
+
+        TimeZone localTimeZone = TimeZone.getDefault();
+
+        TimeZone targetTimeZone = TimeZone.getTimeZone(city);
+
+        long timeDifference = targetTimeZone.getRawOffset() - localTimeZone.getRawOffset();
+
+        long targetTimeMilliseconds = date.getTime() + timeDifference;
+
+        date.setTime(targetTimeMilliseconds);
+
+        modelMap.addAttribute("city", city);
+        modelMap.addAttribute("date", date);
+
+        return "index";
+    }
+}
